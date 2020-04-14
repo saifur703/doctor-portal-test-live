afd5466496050720ddc0a3f86c43bdeed9f82130
Saifur Rahman <saifur703@gmail.com>
2020-04-14 11:59:40 +0600

    Add files via upload

---
+++ precache-manifest.23658ffdde81e02c912d8e64ea56a8cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1bbbdb2355263c864326fe7a6c94d6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc0cb888824ad56e2366",
+    "url": "/static/css/2.949a26d3.chunk.css"
+  },
+  {
+    "revision": "3c54d1751b773620d5a0",
+    "url": "/static/css/main.988c1adf.chunk.css"
+  },
+  {
+    "revision": "fc0cb888824ad56e2366",
+    "url": "/static/js/2.d53c8925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d53c8925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c54d1751b773620d5a0",
+    "url": "/static/js/main.46d370d9.chunk.js"
+  },
+  {
+    "revision": "7c7bb78dff08c093b164",
+    "url": "/static/js/runtime-main.91ea0a20.js"
+  }
+]);
